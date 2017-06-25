abd9340f159f264289ecc86a5a85f06e190a41cd
Brandon Patterson <brandonnpatterson@gmail.com>
2017-06-25 11:59:07 -0500

    set db config

--- config/database.php
+++ config/database.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule;
-mysql://b497412f76e6c8:88a42f8c@us-cdbr-iron-east-03.cleardb.net/heroku_f0c27ae53158310?reconnect=true
 $capsule->addConnection([
   'driver' => 'mysql',
   'host' => 'us-cdbr-iron-east-03.cleardb.net',
