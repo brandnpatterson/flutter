11ae7fd6b436d1d456922103f48420837b731652
Brandon Patterson <brandonnpatterson@gmail.com>
2017-06-25 11:55:32 -0500

    set db config

--- config/database.php
+++ config/database.php
@@ -3,13 +3,13 @@
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule;
-
+mysql://b497412f76e6c8:88a42f8c@us-cdbr-iron-east-03.cleardb.net/heroku_f0c27ae53158310?reconnect=true
 $capsule->addConnection([
   'driver' => 'mysql',
-  'host' => 'localhost',
-  'database' => 'flutter',
-  'username' => 'root',
-  'password' => 'root',
+  'host' => 'us-cdbr-iron-east-03.cleardb.net',
+  'database' => 'heroku_f0c27ae53158310',
+  'username' => 'b497412f76e6c8',
+  'password' => '88a42f8c',
   'charset' => 'utf8',
   'collation' => 'utf8_general_ci'
 ]);
